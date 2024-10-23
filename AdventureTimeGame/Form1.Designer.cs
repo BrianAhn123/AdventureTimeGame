20e367b6e39786f9463d5e99b7edd16e4ace1322
briaahn448 <briaahn448@SDS-L-1VFDF63>
2024-10-23 11:12:04 -0400

    Add project files.

---
+++ AdventureTimeGame/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace AdventureTimeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageBox = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option3Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.BackColor = System.Drawing.Color.Black;
+            this.imageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.imageBox.Font = new System.Drawing.Font("MS PGothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.imageBox.ForeColor = System.Drawing.Color.Lime;
+            this.imageBox.Image = global::AdventureTimeGame.Properties.Resources.adventurelogo;
+            this.imageBox.Location = new System.Drawing.Point(237, 9);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(392, 201);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.Color.Black;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.textBox.Font = new System.Drawing.Font("MS PGothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.ForeColor = System.Drawing.Color.Lime;
+            this.textBox.Location = new System.Drawing.Point(237, 210);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(392, 80);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "Press Button to Start";
+            this.textBox.Click += new System.EventHandler(this.textBox_Click);
+            // 
+            // option1Button
+            // 
+            this.option1Button.Enabled = false;
+            this.option1Button.Location = new System.Drawing.Point(455, 514);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(75, 23);
+            this.option1Button.TabIndex = 2;
+            this.option1Button.Text = "button1";
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.Enabled = false;
+            this.option2Button.Location = new System.Drawing.Point(540, 460);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(75, 23);
+            this.option2Button.TabIndex = 3;
+            this.option2Button.Text = "button2";
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.Enabled = false;
+            this.option3Button.Location = new System.Drawing.Point(455, 433);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(75, 23);
+            this.option3Button.TabIndex = 4;
+            this.option3Button.Text = "button3";
+            this.option3Button.UseVisualStyleBackColor = true;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(540, 326);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "button4";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // option1Label
+            // 
+            this.option1Label.AutoSize = true;
+            this.option1Label.BackColor = System.Drawing.Color.Black;
+            this.option1Label.Font = new System.Drawing.Font("MS PGothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.Lime;
+            this.option1Label.Location = new System.Drawing.Point(245, 247);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(0, 11);
+            this.option1Label.TabIndex = 6;
+            // 
+            // option2Label
+            // 
+            this.option2Label.AutoSize = true;
+            this.option2Label.BackColor = System.Drawing.Color.Black;
+            this.option2Label.Font = new System.Drawing.Font("MS PGothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.Lime;
+            this.option2Label.Location = new System.Drawing.Point(245, 258);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(0, 11);
+            this.option2Label.TabIndex = 7;
+            // 
+            // option3Label
+            // 
+            this.option3Label.AutoSize = true;
+            this.option3Label.BackColor = System.Drawing.Color.Black;
+            this.option3Label.Font = new System.Drawing.Font("MS PGothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3Label.ForeColor = System.Drawing.Color.Lime;
+            this.option3Label.Location = new System.Drawing.Point(245, 269);
+            this.option3Label.Name = "option3Label";
+            this.option3Label.Size = new System.Drawing.Size(0, 11);
+            this.option3Label.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AdventureTimeGame.Properties.Resources.Bmobackground3;
+            this.ClientSize = new System.Drawing.Size(641, 576);
+            this.Controls.Add(this.option3Label);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label imageBox;
+        private System.Windows.Forms.Label textBox;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option3Label;
+    }
+}
+
---
+++ AdventureTimeGame/Form1.cs
@@ -0,0 +1,641 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AdventureTimeGame
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        int deflect = 0;
+        int save = 0;
+        int explode = 0;
+        int flute = 0;
+        int luck;
+        Random Random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            textBox.Text = "Press Small Blue Button to Start!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            option1Button.Enabled = true; 
+            option2Button.Enabled = true;
+            option3Button.Enabled = true;
+            startButton.Enabled = false;
+            if(page == 0)
+            {
+                page = 1;
+            }
+            displayPage();
+        }
+
+        public void displayPage()
+        {
+            switch (page)
+            {
+                case 0:
+                    break;
+                case 1:
+                    textBox.Text = "You Wake up in your Lovely Treehouse Do you want to go on a Adventure or go back to bed?";
+                    option1Label.Text = "Red Button: Go on a Adventure";
+                    option2Label.Text = "Green Button: Go back to bed"; 
+                    break;
+                case 2:
+                    textBox.Text = "You go outside and look at Three Directions which way do you go?";
+                    option1Label.Text = "Red Button: Candy Kingdom";
+                    option2Label.Text = "Green Button: Ice Kingdom";
+                    option3Label.Text = "Blue Triangle Button: Marceline's Cave";
+                    break;
+                case 3:
+                    textBox.Text = "You went back to sleep while jake makes Bacon Pancakes (You took the Tired Route) Play Again? ";
+                    option1Label.Text = "Red Button: Yes";
+                    option2Label.Text = "Green Button: No"; 
+                    break;
+                case 4:
+                    textBox.Text = "You enter the cave and hear Marceline playing her Bass in her home. Will you Interrupt her or Wait?";
+                    option1Label.Text = "Red Button: Interrupt her";
+                    option2Label.Text = "Green Button: Wait";
+                    break;
+                case 5:
+                    textBox.Text = "You enter the Ice kingdom and its quite Chilly and you see Ice King Kidnapping LSP! Sneak Attack or Run at Him?";
+                    option1Label.Text = "Red Button: Sneak Attack";
+                    option2Label.Text = "Green Button: Run at Him";
+                    break;
+                case 6:
+                    textBox.Text = "You enter the Candy Kingdom and see PB drinking some tea. Ask for a Quest or Hang out with her?";
+                    option1Label.Text = "Red Button: Ask for a Quest";
+                    option2Label.Text = "Green Button: Hang out with her";
+                    break;
+                case 7:
+                    textBox.Text = "PB Gave you a quest to Save LSP from the Ice King. Accept or Don't?";
+                    option1Label.Text = "Red Button: Accept the Quest";
+                    option2Label.Text = "Green Button: Don't Accept";
+                    break;
+                case 8:
+                    textBox.Text = "You and PB hang out in the Lab doing Experiements. Do you help her?";
+                    option1Label.Text = "Red Button: Yes";
+                    option2Label.Text = "Green Button: No";
+                    break;
+                case 9:
+                    textBox.Text = "You help her with the Chemicals and the tube starts shaking";
+                    break;
+                case 10:
+                    textBox.Text = "The Tube starts shaking in PB's Hand you run over to save her";
+                    break;
+                case 11:
+                    textBox.Text = "The Tube explodes in her Hand and there was Gum everywhere.(You gained the Failure Route) PLay again?";
+                    break;
+                case 12:
+                    textBox.Text = "You saved her from the explosion and she was glad you were there. (You won the Heroic Ending)";
+                    break;
+                case 13:
+                    textBox.Text = "The chemicals Stabilize and you both create a new Element. (Walter White Ending)";
+                    break;
+                case 14:
+                    textBox.Text = "The Chemicals create a huge explosion and you both died. (Failed Chemist Ending";
+                    break;
+                case 15:
+                    textBox.Text = "You Go in Loud and get frozen in Ice and ice King gets away (Frozen Ending)";
+                    break;
+                case 16:
+                    textBox.Text = "Ice King Gets surprised and knocks you back and charges a Ice Beam Dodge it or Deflect?";
+                    option1Label.Text = "Red Button: Use your Shield to Deflect";
+                    option2Label.Text = "Green Button: Dodge the attack";
+                    break;
+                case 17:
+                    textBox.Text = "You Successfully dodged the attack and prepare to fight back";
+                    option1Label.Text = "Red Button: Use your Fist?";
+                    option2Label.Text = "Green Button: Use your Sword?";
+                    break;
+                case 18:
+                    textBox.Text = "Successfully Deflected the Ice beam and froze the Ice king, Lsp was saved (Heroiz Ending)";
+                    break;
+                case 19:
+                    textBox.Text = "You tried to dodge and get hit by the ice beam turning into a Ice Sphere (Frozen Ending)";
+                    break;
+                case 20:
+                    textBox.Text = "You used your sword but Ice King Freezes it and then Freezes You (Frozen ending)";
+                    break;
+                case 21:
+                    textBox.Text = "You Punch Ice King in the Face Knocking him out. Will you take him to the Candy Kingdom or go Home";
+                    option1Label.Text = "Red Button: Take Ice King to the Candy Kingdom";
+                    option2Label.Text = "Green Button: Go Home";
+                    break;
+                case 22:
+                    textBox.Text = "You take the Ice king to the Candy Kingdom YOU WIN (Ultimate Hero)";
+                    break;
+                case 23:
+                    textBox.Text = "You Go Home leaving the Ice King Behind (Lazy Hero)";
+                    break;
+                case 24:
+                    textBox.Text = "You Interrupt Marceline's Song and she gets pissed at you. Calm her down or do nothing?";
+                    option1Label.Text = "Red Button: Calm her Down";
+                    option2Label.Text = "Green Button: Do Nothing";
+                    break;
+                case 25:
+                    textBox.Text = "You let Marceline play her song and asks you to join her band";
+                    option1Label.Text = "Red Button: Accept to join her band";
+                    option2Label.Text = "Green Button: Don't Accept";
+                    break;
+                case 26:
+                    textBox.Text = "You Calm her down and she continues her song";
+                    break;
+                case 27:
+                    textBox.Text = "You Do Nothing and you get sent to the Nightnosphere where your soul was sucked (Souless Warrior)";
+                    break;
+                case 28:
+                    textBox.Text = "She Shrugs it off and kicks you out of her house (Boring ending)";
+                    break;
+                case 29:
+                    textBox.Text = "You start playing your flute with Marceline";
+                    break;
+                case 30:
+                    textBox.Text = "You play horribly and get kicked out of the band (Mid Musician)";
+                    break;
+                case 31:
+                    textBox.Text = "You play pretty good and Marceline was Impressed. You're getting tired what do you do?";
+                    option1Label.Text = "Red Button: Keep Playing?";
+                    option2Label.Text = "Green Button: Go Home";
+                    break;
+                case 32:
+                    textBox.Text = "You and Marceline keep playing til midnight (Musical Ending)";
+                    break;
+                case 33:
+                    textBox.Text = "You go home and sleep after a long day";
+                    break;
+                case 99:
+                    textBox.Text = "Thank you for playing Adventure TIME!";
+                    Thread.Sleep(2000);
+                    Close();
+                    break;
+                case 69:
+
+                    break;
+                default:
+                    break;
+            }
+        
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            if(page == 1)
+            {
+                page = 2;
+            }
+            else if(page == 2)
+            {
+                page = 6; 
+            }
+            else if (page == 3)
+            {
+                page = 1;
+            }
+            else if (page == 4)
+            {
+                page = 24;
+            }
+            else if (page == 5)
+            {
+                page = 15;
+            }
+            else if (page == 6)
+            {
+                page = 7; 
+            }
+            else if (page == 7)
+            {
+                page = 5;
+            }
+            else if (page == 8)
+            {
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                
+            }
+            else if (page == 10)
+            {
+
+            }
+            else if (page == 11)
+            {
+
+            }
+            else if (page == 12)
+            {
+
+            }
+            else if (page == 13)
+            {
+
+            }
+            else if (page == 14)
+            {
+
+            }
+            else if (page == 15)
+            {
+
+            }
+            else if (page == 16)
+            {
+
+            }
+            else if (page == 17)
+            {
+
+            }
+            else if (page == 18)
+            {
+
+            }
+            else if (page == 19)
+            {
+
+            }
+            else if (page == 20)
+            {
+
+            }
+            else if (page == 21)
+            {
+
+            }
+            else if (page == 22)
+            {
+
+            }
+            else if (page == 23)
+            {
+
+            }
+            else if (page == 24)
+            {
+
+            }
+            else if (page == 25)
+            {
+
+            }
+            else if (page == 26)
+            {
+
+            }
+            else if (page == 27)
+            {
+
+            }
+            else if (page == 28)
+            {
+
+            }
+            else if (page == 29)
+            {
+
+            }
+            else if (page == 30)
+            {
+
+            }
+            else if (page == 31)
+            {
+
+            }
+            else if (page == 32)
+            {
+
+            }
+            else if (page == 33)
+            {
+
+            }
+            else if (page == 34)
+            {
+
+            }
+            else if (page == 69)
+            {
+
+            }
+
+            displayPage();
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+
+            }
+            else if (page == 2)
+            {
+
+            }
+            else if (page == 3)
+            {
+
+            }
+            else if (page == 4)
+            {
+
+            }
+            else if (page == 5)
+            {
+
+            }
+            else if (page == 6)
+            {
+
+            }
+            else if (page == 7)
+            {
+
+            }
+            else if (page == 8)
+            {
+
+            }
+            else if (page == 9)
+            {
+
+            }
+            else if (page == 10)
+            {
+
+            }
+            else if (page == 11)
+            {
+
+            }
+            else if (page == 12)
+            {
+
+            }
+            else if (page == 13)
+            {
+
+            }
+            else if (page == 14)
+            {
+
+            }
+            else if (page == 15)
+            {
+
+            }
+            else if (page == 16)
+            {
+
+            }
+            else if (page == 17)
+            {
+
+            }
+            else if (page == 18)
+            {
+
+            }
+            else if (page == 19)
+            {
+
+            }
+            else if (page == 20)
+            {
+
+            }
+            else if (page == 21)
+            {
+
+            }
+            else if (page == 22)
+            {
+
+            }
+            else if (page == 23)
+            {
+
+            }
+            else if (page == 24)
+            {
+
+            }
+            else if (page == 25)
+            {
+
+            }
+            else if (page == 26)
+            {
+
+            }
+            else if (page == 27)
+            {
+
+            }
+            else if (page == 28)
+            {
+
+            }
+            else if (page == 29)
+            {
+
+            }
+            else if (page == 30)
+            {
+
+            }
+            else if (page == 31)
+            {
+
+            }
+            else if (page == 32)
+            {
+
+            }
+            else if (page == 33)
+            {
+
+            }
+            else if (page == 34)
+            {
+
+            }
+            else if (page == 69)
+            {
+
+            }
+            displayPage();
+        }
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+
+            }
+            else if (page == 2)
+            {
+
+            }
+            else if (page == 3)
+            {
+
+            }
+            else if (page == 4)
+            {
+
+            }
+            else if (page == 5)
+            {
+
+            }
+            else if (page == 6)
+            {
+
+            }
+            else if (page == 7)
+            {
+
+            }
+            else if (page == 8)
+            {
+
+            }
+            else if (page == 9)
+            {
+
+            }
+            else if (page == 10)
+            {
+
+            }
+            else if (page == 11)
+            {
+
+            }
+            else if (page == 12)
+            {
+
+            }
+            else if (page == 13)
+            {
+
+            }
+            else if (page == 14)
+            {
+
+            }
+            else if (page == 15)
+            {
+
+            }
+            else if (page == 16)
+            {
+
+            }
+            else if (page == 17)
+            {
+
+            }
+            else if (page == 18)
+            {
+
+            }
+            else if (page == 19)
+            {
+
+            }
+            else if (page == 20)
+            {
+
+            }
+            else if (page == 21)
+            {
+
+            }
+            else if (page == 22)
+            {
+
+            }
+            else if (page == 23)
+            {
+
+            }
+            else if (page == 24)
+            {
+
+            }
+            else if (page == 25)
+            {
+
+            }
+            else if (page == 26)
+            {
+
+            }
+            else if (page == 27)
+            {
+
+            }
+            else if (page == 28)
+            {
+
+            }
+            else if (page == 29)
+            {
+
+            }
+            else if (page == 30)
+            {
+
+            }
+            else if (page == 31)
+            {
+
+            }
+            else if (page == 32)
+            {
+
+            }
+            else if (page == 33)
+            {
+
+            }
+            else if (page == 34)
+            {
+
+            }
+            else if (page == 69)
+            {
+
+            }
+            displayPage();
+        }
+
+        private void textBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
